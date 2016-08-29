98ce23533d7eaa5638c9406c54224208cbb76495
sorrow-91 <antoniom.diaz.dom@gmail.com>
2016-08-29 22:57:10 +0200

    Updated AngularJS to 1.5.5

--- public/js/controllers/MainController.js
+++ public/js/controllers/MainController.js
@@ -11,4 +11,20 @@ app.controller('mainController',function($scope){
 
     layer.setVisible(toggle);
   }
+
+  $scope.queryDatabase = function() {
+	$mdDialog.show(
+	$mdDialog.alert()
+		.clickOutsideToClose(true)
+		.title('Opening from the left')
+		.textContent('Closing to the right!')
+		.ariaLabel('Left to right demo')
+		.ok('Nice!')
+        // You can specify either sting with query selector
+		.openFrom('#left')
+        // or an element
+		.closeTo(angular.element(document.querySelector('#right')))
+    );
+  };
 });
+
